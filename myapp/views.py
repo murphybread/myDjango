8d5d0d688a5b2ffbff41398d7d5436007d12d75e
Murphybread <nargene@gmail.com>
2023-12-16 00:06:13 +0900

    feat: 홈페이지 CRUD의 R기능 구현 동적 처리

--- myapp/views.py
+++ myapp/views.py
@@ -2,9 +2,35 @@ from django.shortcuts import render, HttpResponse
 import random
 
 
+topics = [
+    {"id": 1, "title": "roting", "body": "Routing is ..."},
+    {"id": 2, "title": "view", "body": "View is ..."},
+    {"id": 3, "title": "moel", "body": "Model is ..."},
+]
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Random : </h1>" + str(random.random()))
+    global topics
+    ol = ""
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return HttpResponse(
+        f"""
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            {ol}
+        </ol>
+        <h2> Welcom </h2>
+        Hello, Django
+    
+    
+    </body>
+    </html>
+    """
+    )
 
 
 def create(request):
